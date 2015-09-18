2b7316f49e72aef6344707f2bb3296f6ae966a41
Demo User <ubuntu@arm.localdomain>
2015-09-18 05:27:40 +0000

    modified settings.py

--- circling/circling/wsgi.py
+++ circling/circling/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cycling project.
+WSGI config for circling project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cycling.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "circling.settings")
 
 application = get_wsgi_application()
